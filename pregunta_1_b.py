46ada7f28f52633cc762e2ebebac8b1ec371801f
Ker0V1 <kennyrojas27@gmail.com>
2025-02-10 10:31:06 -0400

    - Completando JSON

--- pregunta_1_b.py
+++ pregunta_1_b.py
@@ -2,22 +2,10 @@
 Ejemplo de parseo de JSON basado en el ejemplo de la libreria parsec.py 
 
 https://github.com/sighingnow/parsec.py/blob/master/examples/jsonc.py
-
-El lenguaje que queremos parsear es el siguiente:
-    S -> J
-    J -> {L}
-    L -> s:VR
-    R -> ,s:VR | lambda
-    V -> s | n | J | A
-    A -> [X]
-    X -> VY
-    Y -> ,VY | lambda
-con los simbolos terminales: s, n, [, ], {, }, :, ,
 '''
 
 import re
 from parsec import *
-import sys
 
 # eliminamos los espacios en blanco
 white_space = regex(r'\s*', re.MULTILINE)
@@ -25,8 +13,6 @@ def lexme(p): return p << white_space
 
 
 # definimos los simbolos terminales
-s_token = lexme(string('s'))
-n_token = lexme(string('n'))
 open_bracket = lexme(string('['))
 close_bracket = lexme(string(']'))
 open_brace = lexme(string('{'))
@@ -34,30 +20,85 @@ close_brace = lexme(string('}'))
 colon = lexme(string(':'))
 comma = lexme(string(','))
 
-# definimos las reglas
+true = lexme(string('true')).result(True)
+false = lexme(string('false')).result(False)
+null = lexme(string('null')).result(None)
 
+
+def number():
+    '''
+    Parsea un numero, este puede ser enteros o flotantes
+    '''
+    return lexme(regex(r'-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?')).parsecmap(float)
+
+
+def char_seq():
+    '''
+    Parsea un Cadena de caracteres con caracteres normales y caracteres escapados
+    '''
+
+    def normal_char():
+        '''
+        Una cadena de caracteres normales sin comillas ni barras
+        '''
+        return regex(r'[^"\\]+')
+
+    def escp_char():
+        '''
+        Caracteres escapados dentro de una cadena de caracteres
+        '''
+        
+        return string('\\') >> (
+                string('"') 
+                | string('\\') 
+                | string('/') 
+                | string('b').result('\b') 
+                | string('f') .result('\f')
+                | string('n').result('\n')  
+                | string('r').result('\r')
+                | string('t').result('\t')
+                | regex(r'u[0-9a-fA-F]{4}')).parsecmap(lambda x: chr(int(x[1:], 16)))
+    
+    return normal_char() | escp_char()
+
+
+# definimos las reglas
+@lexme
+@generate
+def quoted():
+    '''
+    Parsea una cadena de caracteres entre comillas
+    '''
+    yield string('"')
+    result = yield many(char_seq())
+    yield string('"')
+    return ''.join(result)
 
 @generate
-def object_json():
+def json_object():
     '''
-    J -> {L}
+    Parsea  un objeto JSON 
     '''
     yield open_brace
-    l = yield object_list
+    l = yield optional(object_list, None)
     yield close_brace
+    
+    if l is None:
+        return []
+    
     return l
 
 
 @generate
 def object_list():
     '''
-    L -> s:VR
+    Parsea una lista de objetos que van contenidos en un objeto JSON
     '''
-    s = yield s_token
+    s = yield quoted
     yield colon
     v = yield value
     result = [(s, v)]
-    
+
     # Si R es lambda, entonces no se hace nada
     r = yield optional(object_list_tail, None)
     if r:
@@ -68,52 +109,56 @@ def object_list():
 @generate
 def object_list_tail():
     '''
-    R -> ,s:VR | lambda
+    Funcion auxiliar para parsear una lista de objetos contenidos en un objeto JSON
     '''
     yield comma
-    s = yield s_token
+    s = yield quoted
     yield colon
     v = yield value
     result = [(s, v)]
-    
+
     # Si R es lambda, entonces no se hace nada
     r = yield optional(object_list_tail, None)
     if r:
         result += r
-    
+
     return result
 
 
 @generate
 def value():
     '''
-    V -> s | n | J | A
+    Parsea los valores que pueden ir a la derecha de un par clave-valor en un objeto JSON
     '''
-    v = yield s_token | n_token | object_json | array_json
+    v = yield quoted | number() | json_object | array_json
     return v
 
 
 @generate
 def array_json():
     '''
-    A -> [X]
+    Parsea un arreglo vacio y con contenido
     '''
     yield open_bracket
-    x = yield array_list
+    x = yield optional(array_list, None)
     yield close_bracket
+
+    if x is None:
+        return []
+
     return x
 
 
 @generate
 def array_list():
     '''
-    X -> VY
+    Parsea una lista de valores que van dentro de un arreglo JSON
     '''
     v = yield value
     result = [v]
-    
+
     # Si Y es lambda, entonces no se hace nada
-    y= yield optional(array_list_tail, None)
+    y = yield optional(array_list_tail, None)
     if y:
         result += y
     return result
@@ -122,24 +167,29 @@ def array_list():
 @generate
 def array_list_tail():
     '''
-    Y -> ,VY | lambda
+    Funcion auxiliar para parsear una lista de valores que van dentro de un arreglo JSON
     '''
     yield comma
     v = yield value
     result = [v]
-    y= yield optional(array_list_tail, None)
+    y = yield optional(array_list_tail, None)
     if y:
         result += y
     return result
 
 
-# V -> s | n | J | A
-value = s_token | n_token | object_json | array_json
+# Los valores posibles de un JSON
+value = quoted | number() | json_object | array_json | true | false | null
+
+# El parser de json
+
+json = white_space >> json_object
 
 if __name__ == '__main__':
     input_str = input("Introduce la frase en JSON que quiere parsear: ")
     try:
-        result = object_json.parse(input_str)
+        result = json_object.parse(input_str)
         print("Fino, se parseo bien la frase, el resultado es: ", result)
     except Exception as e:
-        print(f"No se pudo parsear correctamente la frase dio el error: \n{e}.")
+        print(
+            f"No se pudo parsear correctamente la frase dio el error: \n{e}.")
--- test_pregunta_1_b.py
+++ test_pregunta_1_b.py
@@ -1,31 +1,46 @@
 import pytest
-from pregunta_1_b import object_json
+from pregunta_1_b import json
 
 
-def test_simple_object():
-    json_str = '{s:n}'
-    result = object_json.parse(json_str)
-    assert result == [('s', 'n')]
-
+def test_simple_objects():
+    result = json.parse('{"s":"hola mundo"}')
+    assert result == [("s", "hola mundo")]
+    
+    result = json.parse('{"entero": 10}')
+    assert result == [("entero", 10)]
+    
+    result = json.parse('{"flotante": 3.1416}')
+    assert result == [("flotante", 3.1416)]
+    
+    result = json.parse('{"Velda": true}')
+    assert result == [('Velda', True)]
+    
+    result = json.parse('{"Falso": false}')
+    assert result == [('Falso', False)]
+    
+    result = json.parse('{"Nothing": null}')
+    assert result == [('Nothing', None)]
 
 def test_nested_object():
-    json_str = '{s:{s:n}}'
-    result = object_json.parse(json_str)
-    assert result == [('s', [('s', 'n')])]
-
+    result = json.parse('{"primer nivel":{"segundo nivel": "fin"}}')
+    assert result == [('primer nivel', [('segundo nivel', 'fin')])]
 
 def test_array_in_object():
-    json_str = '{s:[n, s]}'
-    result = object_json.parse(json_str)
-    assert result == [('s', ['n', 's'])]
+    result = json.parse('{"Arreglo":["item", 10, 3.14]}')
+    assert result == [('Arreglo', ['item', 10, 3.14])]
+
+def test_empty_object():
+    result = json.parse('{}')
+    assert result == []
+
+def test_empty_array():
+    result = json.parse('{"arreglo vacio": []}')
+    assert result == [("arreglo vacio", [])]
 
 
 def test_complex_object():
-    json_str = '{s:n, s:s, s:{s:n}, s:[n, s]}'
-    result = object_json.parse(json_str)
-    assert result == [('s', 'n'), ('s', 's'),
-                      ('s', [('s', 'n')]), ('s', ['n', 's'])]
-
+    result = json.parse('{"s_1":3.1416, "s_2":"s_2_1", "s_3":{"s_3_1":10}, "s_4":[25, "s_4_1"]}')
+    assert result == [('s_1', 3.1416), ('s_2', 's_2_1'), ('s_3', [('s_3_1', 10.0)]), ('s_4', [25.0, 's_4_1'])]
 
 if __name__ == '__main__':
     pytest.main()
